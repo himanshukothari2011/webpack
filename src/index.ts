d99d16678695c541ec0c2a3143381f4d9be2a5a4
himanshukothari2011 <himanshukothari1987@gmail.com>
2021-06-21 18:27:02 +0530

    All webpack

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("HIIII Himanshu Kothari this is my");
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+//node programming
+const path = require('path');
+module.exports ={
+    entry: "./src/index.ts",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname,'dist'),
+        publicPath:'dist'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules:[
+            {
+                test: /\.ts$/,
+                use:'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+   resolve:{
+       extensions:['.ts','.js']
+   }
+};
